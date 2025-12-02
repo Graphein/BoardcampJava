33c9995a027ac5a542d416fe53ffdd82e2760d7a
Eduardo Leli <135680059+Graphein@users.noreply.github.com>
2025-12-02 15:14:45 -0300

    chore: final project

--- src/main/java/com/eduleli/boardcamp/BoardcampApplication.java
+++ src/main/java/com/eduleli/boardcamp/BoardcampApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BoardcampApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BoardcampApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(BoardcampApplication.class, args);
+    }
 }
